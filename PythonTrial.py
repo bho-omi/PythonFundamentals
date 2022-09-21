7d653b09f9f60a3d376037a7c976a4a7621a9c8c
bho-omi <113066031+bho-omi@users.noreply.github.com>
2022-09-21 19:29:36 -0400

    Weight Converter

--- PythonTrial.py
+++ PythonTrial.py
@@ -1,1 +1,8 @@
-print("Hello World")
+Weight = float(input("Weight: "))
+Unit = input("(K)gs or (L)bs: ")
+if Unit.upper() == "K":
+    converted = Weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = Weight * 0.45
+    print("Weight in Kgs: " + str(converted))
